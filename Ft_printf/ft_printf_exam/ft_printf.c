0bfae49a6cb4ad18bd80d9e6aff9cdfdc5e4f8c6
rcutte <rcutte@student.42.fr>
2024-02-14 23:46:04 +0000

    shortened ft_printf exam

--- Ft_printf/ft_printf_exam/ft_printf.c
+++ Ft_printf/ft_printf_exam/ft_printf.c
@@ -6,12 +6,12 @@
 /*   By: rcutte <rcutte@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/12 22:31:29 by rcutte            #+#    #+#             */
-/*   Updated: 2024/02/12 22:32:31 by rcutte           ###   ########.fr       */
+/*   Updated: 2024/02/14 23:42:04 by rcutte           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdarg.h>
 #include <unistd.h>
+#include <stdarg.h>
 #include <stdlib.h>
 
 int	ft_putchar(char c)
@@ -24,11 +24,14 @@ int	ft_putchar(char c)
 
 int	ft_putstr(char *str)
 {
-	int	count;
+	int count;
 
 	count = 0;
 	if (!str)
-		return (ft_putstr("(null)"));
+	{
+		count += write(1, "(null)", 6);
+		return (count);
+	}
 	while (*str)
 	{
 		count += ft_putchar(*str);
@@ -37,109 +40,99 @@ int	ft_putstr(char *str)
 	return (count);
 }
 
-int	ft_putnbr(int n)
+int	ft_putnbr(int nb)
 {
-	long long	ln;
-	int		count;
-	int		lim;
-	int		sign;
-	char	*str;
+	char nb_return[12];
+	int i;
+	int count;
+	long long number;
 
 	count = 0;
-	sign = 0;
-	ln = (long long)n;
-	str = malloc(12);
-	if (ln == 0)
+	number = nb;
+	if (number == 0)
 	{
 		count += ft_putchar('0');
 		return (count);
 	}
-	if (ln < 0)
+	if (number < 0)
 	{
 		count += ft_putchar('-');
-		sign = -1;
-		ln *= -1;
+		number *= -1;
 	}
-	while (ln > 0)
+	i = 0;
+	while (number != 0)
 	{
-		str[count] = ln % 10 + '0';
-		ln /= 10;
-		count++;
+		nb_return[i] = number % 10 + '0';
+		number = number / 10;
+		i++;
 	}
-	int tmp = count;
-	if (sign == -1)
-		lim = 1;
-	else
-		lim = 0;
-	while (count > 0 + lim)
+	nb_return[i] = '\0';
+	while (i > 0)
 	{
-		count--;
-		ft_putchar(str[count]);
+		i--;
+		count += ft_putchar(nb_return[i]);
 	}
-	free(str);
-	return (tmp);
+	return (count);
 }
 
-int ft_puthex(unsigned int n)
+int	ft_puthex(unsigned int nb)
 {
-	int		count;
-	char	*str;
-	char	*hex;
+	char nb_return[12];
+	int	count;
+	int	i;
 
+	i = 0;
 	count = 0;
-	str = malloc(12);
-	hex = "0123456789abcdef";
-	if (n == 0)
+	char hex[] = "0123456789abcdef";
+	if (nb == 0)
 	{
 		count += ft_putchar('0');
 		return (count);
 	}
-	while (n > 0)
+	while (nb != 0)
 	{
-		str[count] = hex[n % 16];
-		n /= 16;
-		count++;
+		nb_return[i] = hex[nb % 16];
+		nb = nb / 16;
+		i++;
 	}
-	int tmp = count;
-	while (count > 0)
+	nb_return[i] = '\0';
+	while (i > 0)
 	{
-		count--;
-		ft_putchar(str[count]);
+		i--;
+		count += ft_putchar(nb_return[i]);
 	}
-	free(str);
-	return (tmp);
+	return (count);
 }
 
 int	ft_printf(const char *format, ...)
 {
-	int		count;
-	va_list	args;
+	va_list ptr;
+	int 	count;
 
-	va_start(args, format);
 	count = 0;
 	if (!format)
 		return (-1);
+	va_start(ptr, format);
 	while (*format)
 	{
 		if (*format == '%')
 		{
 			format++;
-			if (*format == 'd')
-				count += ft_putnbr(va_arg(args, int));
-			else if (*format == 's')
-				count += ft_putstr(va_arg(args, char *));
-			else if (*format == 'c')
-				count += ft_putchar(va_arg(args, int));
+			if (*format == 's')
+				count += ft_putstr(va_arg(ptr, char *));
+			else if (*format == 'd')
+				count += ft_putnbr(va_arg(ptr, int));
 			else if (*format == 'x')
-				count += ft_puthex(va_arg(args, unsigned int));
+				count += ft_puthex(va_arg(ptr, unsigned int));
 			else if (*format == '%')
 				count += ft_putchar('%');
 		}
 		else
+		{
 			count += ft_putchar(*format);
+		}
 		format++;
 	}
-	va_end(args);
 	return (count);
 }
 

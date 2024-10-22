fcc02eaa15c1477395de9473c73f8fcdb8d6afeb
rcutte <rcutte@student.42.fr>
2024-10-22 23:51:53 +0200

    feat: script for auto management of django exo

--- Projects/Django/2-ORM/django_setup.sh
+++ Projects/Django/2-ORM/django_setup.sh
@@ -13,15 +13,18 @@ RESET='\033[0m'
 
 venv_path=".venv"
 project_name="d06"
+database_folder="db"
 
 # Function to display help message
 show_help() {
   echo -e "${CYAN}Usage:${RESET}"
   echo -e "${GREEN}  source ./my_script.sh <launch|env|django|help>${RESET}"
   echo -e "${CYAN}Commands:${RESET}"
-  echo -e "${YELLOW}  launch${RESET}   : Setup Python env and Run Django project"
+  echo -e "${YELLOW}  launch${RESET}   : Setup Docker containers, Python env and Run Django project"
   echo -e "${YELLOW}  env${RESET}      : Setup Python environment (install virtualenv, dependencies)"
   echo -e "${YELLOW}  django${RESET}   : Run the Django project (migrate, collectstatic, runserver)"
+  echo -e "${YELLOW}  docker${RESET}   : Setup Docker containers (postgresql and adminer)"
+  echo -e "${YELLOW}  clean${RESET}    : Remove every containers, images, volumes, data, env"
   echo -e "${YELLOW}  help${RESET}     : Display this help message"
 }
 
@@ -31,26 +34,91 @@ setup_env() {
   python3 -m venv $venv_path
   source $venv_path/bin/activate
   
-  echo "=============={🧰 ${CYAN}Pip Upgrade${RESET} 🧰}=============="
+  echo -e "=============={🧰 ${CYAN}Pip Upgrade${RESET} 🧰}=============="
   pip install --upgrade pip
 
-  echo "=============={🧰 ${CYAN}Path Module Installation${RESET} 🧰}=============="
+  echo -e "=============={🧰 ${CYAN}Path Module Installation${RESET} 🧰}=============="
   pip install --upgrade -r requirements.txt
 }
 
 # Function to run the Django project
 run_django() {
-  echo "=============={🚀 ${CYAN}Program Launch${RESET} 🚀}=============="
+  echo -e "=============={🚀 ${CYAN}Program Launch${RESET} 🚀}=============="
   cd $project_name
-  python3 manage.py makemigration
+  python3 manage.py makemigrations
   python3 manage.py migrate
   python3 manage.py collectstatic --no-input
   python3 manage.py runserver
 }
 
+# Function to setup Docker containers
+run_docker(){
+    echo -e "=============={🐳 ${CYAN}Containers Launch${RESET} 🐳}=============="
+    docker compose up -d
+    if [ $? -eq 0 ]; then
+        echo -e "${GREEN}Docker containers launched successfully.${RESET}"
+    else
+        echo -e "${RED}Failed to launch Docker containers.${RESET}"
+        exit 1
+    fi
+}
+
+# Function to clean up the program env
+cleanup() {
+    echo -e "=============={🧹 ${CYAN}Cleaning Up${RESET} 🧹}=============="
+    
+    # Stop and remove Docker containers
+    docker compose down
+    if [ $? -eq 0 ]; then
+            echo -e "${GREEN}Docker containers stopped and removed successfully.${RESET}"
+    else
+            echo -e "${RED}Failed to stop and remove Docker containers.${RESET}"
+    fi
+
+    # Remove Docker images
+    docker rmi $(docker images -q)
+    if [ $? -eq 0 ]; then
+            echo -e "${GREEN}Docker images removed successfully.${RESET}"
+    else
+            echo -e "${RED}Failed to remove Docker images.${RESET}"
+    fi
+
+    # Remove Docker volumes
+    docker volume prune -f
+    if [ $? -eq 0 ]; then
+            echo -e "${GREEN}Docker volumes removed successfully.${RESET}"
+    else
+            echo -e "${RED}Failed to remove Docker volumes.${RESET}"
+    fi
+    # Remove Database
+    if [ -d  "$database_folder" ]; then
+        rm -rf $database_folder
+        if [ $? -eq 0 ]; then
+                echo -e "${GREEN}Database folder removed successfully.${RESET}"
+        else
+                echo -e "${RED}Failed to remove Database folder.${RESET}"
+        fi
+    else
+        echo -e "${YELLOW}No Database folder found.${RESET}"
+    fi
+
+    # Deactivate and remove Python virtual environment
+    if [ -d "$venv_path" ]; then
+        rm -rf $venv_path
+        if [ $? -eq 0 ]; then
+                echo -e "${GREEN}Python virtual environment removed successfully.${RESET}"
+        else
+                echo -e "${RED}Failed to remove Python virtual environment.${RESET}"
+        fi
+    else
+        echo -e "${YELLOW}No Python virtual environment found.${RESET}"
+    fi
+}
+
 # Handle script arguments
 case "$1" in
   launch)
+    run_docker
     setup_env
     run_django
     ;;
@@ -59,6 +127,12 @@ case "$1" in
     ;;
   django)
     run_django
+    ;;
+  docker)
+    run_docker
+    ;;
+  clean)
+    cleanup
     ;;
   help)
     show_help

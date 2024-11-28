e163447eefce8b9a1442e0ea5452cb889dd55298
rcutte <rcutte@student.42.fr>
2024-11-28 17:13:53 +0100

    !feat: add chat app early files
    no routes setup

--- Projects/Django/3-Final/d09/d09/urls.py
+++ Projects/Django/3-Final/d09/d09/urls.py
@@ -23,4 +23,5 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('account.urls')),
+    path('chat/', include('chat.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

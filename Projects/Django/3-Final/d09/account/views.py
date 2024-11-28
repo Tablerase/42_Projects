f34833b1e1638c737ce5a123883e62fbf39c320e
rcutte <rcutte@student.42.fr>
2024-11-28 14:56:34 +0100

    !feat: working ajax but csrf security struggle

--- Projects/Django/3-Final/d09/account/views.py
+++ Projects/Django/3-Final/d09/account/views.py
@@ -4,11 +4,18 @@ from django.http import JsonResponse
 from django.views.generic import View
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.views import LoginView as DjangoLoginView, LogoutView as DjangoLogoutView
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+
+def login_status(request):
+    return JsonResponse({'is_logged_in': request.user.is_authenticated})
 
 
 class LoginView(DjangoLoginView):
     authentication_form = AuthenticationForm
     template_name = 'login.html'
+    success_url = '/account'
 
     def form_valid(self, form):
         user = form.get_user()
@@ -24,7 +31,15 @@ class LoginView(DjangoLoginView):
         return super().form_invalid(form)
 
 
-class LogoutView(DjangoLogoutView):
+class LogoutView(LoginRequiredMixin, DjangoLogoutView):
+    login_url = '/account'
+
+    def get(self, request, *args, **kwargs):
+        if self.request.headers.get('x-requested-with') == 'XMLHttpRequest':
+            csrf_token = request.META.get('CSRF_COOKIE', None)
+            return JsonResponse({'csrf_token': csrf_token})
+        return super().get(request, *args, **kwargs)
+
     def post(self, request, *args, **kwargs):
         logout(request)
         if self.request.headers.get('x-requested-with') == 'XMLHttpRequest':
